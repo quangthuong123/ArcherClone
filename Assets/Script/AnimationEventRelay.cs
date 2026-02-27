c610ef7285d30d1205f851cd1c376f62581d050a
quangthuong123 <125529148+quangthuong123@users.noreply.github.com>
2026-02-27 17:11:22 +0700

    fix 1.0

--- Assets/Script/AnimationEventRelay.cs
+++ Assets/Script/AnimationEventRelay.cs
@@ -15,6 +15,17 @@ public class AnimationEventRelay : MonoBehaviour
     {
         if (parentScript != null) parentScript.SpawnArrow();
     }
+    public void PerformAttack()
+    {
+        // Safely find the EnemyAI script on the parent object
+        EnemyAI aiScript = GetComponentInParent<EnemyAI>();
+
+        if (aiScript != null)
+        {
+            // Send the message DIRECTLY to the AI's object, skipping this object entirely!
+            aiScript.gameObject.SendMessage("PerformAttack", SendMessageOptions.DontRequireReceiver);
+        }
+    }
 
     // Add these empty functions to mute the asset pack's leftover sound events
     public void FootL() { }
